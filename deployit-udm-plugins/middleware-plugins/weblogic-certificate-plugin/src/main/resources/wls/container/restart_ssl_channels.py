f48c04c6ed66bde3f89f33c03f80c79c5b17b91f
Fai Fung <ffung@xebia.com>
2014-08-14 14:49:12 +0200

    don't reuse ensureStableContainerState, it doesn't handle errors nicely and restarting SSL only works on a Server anyway

--- deployit-udm-plugins/middleware-plugins/weblogic-certificate-plugin/src/main/resources/wls/container/restart_ssl_channels.py
+++ deployit-udm-plugins/middleware-plugins/weblogic-certificate-plugin/src/main/resources/wls/container/restart_ssl_channels.py
@@ -1,33 +1,27 @@
+def ensureStableServerState(server):
+    for a in range(1, 5):
+        status = cmo.lookupServerLifeCycleRuntime(server.name).getState()
+        print "Server: ", server.name, ": ", status
+
+        if (status not in ['SHUTTING_DOWN', 'STARTING']):
+            return True
+
+        print "# ", a, " Sleeping 2 seconds"
+        java.lang.Thread.sleep(2000)
+
+    return False
+
 domainRuntime()
 
-
-# check if function is available otherwise define it (workaround for OSB) 
-if 'ensureStableContainerState' not in dir():
-    def ensureStableContainerState(target, type):
-        print "Going to check state of: ", type, " ", target.name
-    
-        if type == "Server": 
-            servers = [target.name]
-        else: 
-            servers = target.servers
-    
-        for name in servers:
-            for a in range(1, 5):
-                status = cmo.lookupServerLifeCycleRuntime(name).getState()
-                print type, ": ", name, ": ",status
-    
-                if (status not in ['SHUTTING_DOWN', 'STARTING']):
-                    return
-    
-                print "# ", a, " Sleeping 2 seconds"
-                java.lang.Thread.sleep(2000)
-                
-ensureStableContainerState(target, type)
-        
-if exists('/ServerRuntimes/' + target.name):
-    print 'Restarting SSL Channels for ' + target.name
-    cd ('/ServerRuntimes/' + target.name)
-    cmo.restartSSLChannels()
-    print 'Restarted SSL Channels for ' + target.name
+if ensureStableServerState(target):
+    if exists('/ServerRuntimes/' + target.name):
+        print 'Restarting SSL Channels for ' + target.name
+        cd ('/ServerRuntimes/' + target.name)
+        cmo.restartSSLChannels()
+        print 'Restarted SSL Channels for ' + target.name
+    else:
+        print 'Restarting SSL Channels for ' + target.name + ' is not necessary, target is down'
 else:
-    print 'Restarting SSL Channels for ' + target.name + ' is not necessary, target is down'
+    print 'Waiting for a stable server state has timed out, the server ' + target.name + ' is still SHUTTING DOWN or STARTING'
+    print 'Restarting SSL Channels failed'
+    sys.exit(1)
